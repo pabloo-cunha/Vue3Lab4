539f31828e341c5ace69918754cdeb9ff8a931d7
pabloo-cunha <pablo.limacunha@outlook.com>
2024-03-13 22:39:24 -0300

    component Usuario

---
+++ vue-project-bd4/src/components/TabelaUsuario.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  usuarios: { id: number; nome: string }[]
+}>()
+</script>
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <td>Id</td>
+          <td>Nome</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios" :key="usuario.id">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- Seu código HTML aqui -->
+  </div>
+</template>
---
+++ vue-project-bd4/src/views/UsuarioView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Olá, {{ nome }}</h1>
+    <input type="text" v-model="nome" />
+    <p v-if="nome.length > 10">Nome muito longo</p>
+    <p v-else-if="nome.length < 5">Nome curto</p>
+    <TabelaUsuario :usuarios="usuarios"/>
+    <!-- <table>
+      <thead>
+        <tr>
+          <td>Id</td>
+          <td>Nome</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios" :key="usuario.id">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+        </tr>
+      </tbody>
+    </table> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TabelaUsuario from '@/components/TabelaUsuario.vue'
+
+const nome = ref('Teste')
+const usuarios = ref([
+  { id: 1, nome: 'Admin' },
+  { id: 2, nome: 'User' }
+])
+</script>
